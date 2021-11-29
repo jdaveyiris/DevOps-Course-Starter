4a95ca926b2001fb919129f815cbdb32fab2e9e3
Jim Davey <jim.davey@iris.co.uk>
2021-11-29 11:36:22 +0000

    Display List

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from todo_app.data.session_items import get_items
 
 from todo_app.flask_config import Config
 
@@ -8,4 +10,5 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items=items)
