1373aa469532b6eacae5a1465d860639e97eda45
Jim Davey <jim.davey@iris.co.uk>
2021-11-29 16:52:47 +0000

    Stretch Goals

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
-from todo_app.data.session_items import save_item, get_items, add_item
+from todo_app.data.session_items import save_item, get_items, add_item, get_item
 from flask import request, redirect
+#from flask_bootstrap import Bootstrap
 
 from todo_app.flask_config import Config
 
@@ -9,9 +10,17 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     items = get_items()
+    
+    if request.method == "POST":
+        id = request.form.getlist("item.id")
+         
+        for x in id:
+            items[int(x)-1].update({"status": "Complete"})
+            save_item(items[int(x)-1])
+
     return render_template('index.html', items=items)
 
 @app.route('/add', methods=['GET', 'POST'])
